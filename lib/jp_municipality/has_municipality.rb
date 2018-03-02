2d1d550ae0fcd71c2f6b6b017321cd9229c348af
h-izumi <h-izumi@9uelle.jp>
2018-03-02 20:03:36 +0900

    `has_municipality`: Set 'optional: true' as default.

--- lib/jp_municipality/has_municipality.rb
+++ lib/jp_municipality/has_municipality.rb
@@ -4,8 +4,11 @@ module JpMunicipality
 
     module ClassMethods
       def has_municipality(foreign_key = :municipality_code, options = {})
-        options.reverse_merge!({ as: :municipality })
-        belongs_to options[:as], foreign_key: foreign_key,
+        options.reverse_merge!({
+          as: :municipality,
+          optional: true,
+        })
+        belongs_to options[:as], foreign_key: foreign_key, optional: options[:optional],
           class_name: 'JpMunicipality::Municipality'
       end
     end
