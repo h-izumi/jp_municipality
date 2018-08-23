fb234bbf8b80e45236053d6881dff597e692d315
h-izumi <h-izumi@9uelle.jp>
2018-08-23 11:29:52 +0900

    add columns for district

--- db/migrate/20180226073252_create_municipalities.rb
+++ db/migrate/20180226073252_create_municipalities.rb
@@ -2,8 +2,12 @@ class CreateMunicipalities < ActiveRecord::Migration[5.2]
   def up
     create_table :municipalities, id: :string, primary_key: :code do |t|
       t.string :prefecture_code, null: false, limit: 2
+      t.string :district_name
       t.string :name
+      t.string :district_kana
       t.string :kana
+      t.string :district_name_e
+      t.string :name_e
     end
 
     change_column :municipalities, :code, :string, limit: 6
