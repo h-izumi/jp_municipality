48b2fb60c6617208145b827073a610b8fbe24b8d
h-izumi <h-izumi@9uelle.jp>
2018-08-23 13:11:57 +0900

    Refactoring migration

--- db/migrate/20180226073252_create_municipalities.rb
+++ db/migrate/20180226073252_create_municipalities.rb
@@ -1,6 +1,6 @@
 class CreateMunicipalities < ActiveRecord::Migration[5.2]
   def up
-    create_table :municipalities, id: :string, primary_key: :code do |t|
+    create_table :municipalities, id: :string, primary_key: :code, limit: 6 do |t|
       t.string :prefecture_code, null: false, limit: 2
       t.string :district_name
       t.string :name
@@ -10,7 +10,6 @@ class CreateMunicipalities < ActiveRecord::Migration[5.2]
       t.string :name_e
     end
 
-    change_column :municipalities, :code, :string, limit: 6
     add_index :municipalities, :prefecture_code
   end
 
